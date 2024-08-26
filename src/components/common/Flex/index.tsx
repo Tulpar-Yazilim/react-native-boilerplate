bf82e64f15ef5e2b432b2ddb05b75e9f91a3c6e5
Kadir Barcın <47914558+CruzNadin@users.noreply.github.com>
2024-08-26 15:51:08 +0300

    flex component

--- src/components/common/Flex/index.tsx
+++ src/components/common/Flex/index.tsx
@@ -1,12 +1,12 @@
 import React, {FunctionComponent} from 'react';
 import {View} from 'react-native';
 
-import {flexBoxStyle} from './styles';
-import {FlexProps} from './type';
+import styles, {flexBoxStyle} from './styles';
+import type {FlexProps} from './type';
 
 export const Flex: FunctionComponent<FlexProps> = ({flex, direction, gap, justify, align}) => {
   return (
-    <View style={{height: 200, width: '100%'}}>
+    <View style={[{height: 200, width: '100%'}, styles.main]}>
       <View style={flexBoxStyle({flex, direction, gap, justify, align})}>
         <View style={{backgroundColor: 'red', flex: 1, width: 40, height: 40}} />
         <View style={{backgroundColor: 'yellow', flex: 1, width: 40, height: 40}} />
--- src/components/common/Flex/styles.ts
+++ src/components/common/Flex/styles.ts
@@ -1,4 +1,14 @@
+import {StyleSheet} from 'react-native';
+
 import {FlexProps} from './type';
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+  },
+});
+
+export default styles;
 
 export const flexBoxStyle = ({flex, direction, gap, justify, align}: FlexProps) => {
   if (typeof flex !== 'number') console.warn('Flex is not number');
