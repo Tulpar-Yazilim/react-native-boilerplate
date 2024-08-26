30fe2f6e5094fde8911043c190086d3727d526f3
Kadir Barcın <47914558+CruzNadin@users.noreply.github.com>
2024-08-26 17:09:21 +0300

    flex component edited

--- src/components/common/Flex/index.tsx
+++ src/components/common/Flex/index.tsx
@@ -2,17 +2,12 @@ import React, {FunctionComponent} from 'react';
 import {View} from 'react-native';
 
 import styles, {flexBoxStyle} from './styles';
-import type {FlexProps} from './type';
+import type {FlexViewProps} from './type';
 
-export const Flex: FunctionComponent<FlexProps> = ({flex, direction, gap, justify, align}) => {
+export const Flex: FunctionComponent<FlexViewProps> = ({flex, direction, gap, justify, align, ...props}) => {
   return (
-    <View style={[{height: 200, width: '100%'}, styles.main]}>
-      <View style={flexBoxStyle({flex, direction, gap, justify, align})}>
-        <View style={{backgroundColor: 'red', flex: 1, width: 40, height: 40}} />
-        <View style={{backgroundColor: 'yellow', flex: 1, width: 40, height: 40}} />
-        <View style={{backgroundColor: 'green', flex: 1, width: 40, height: 40}} />
-        <View style={{backgroundColor: 'purple', flex: 1, width: 40, height: 40}} />
-      </View>
+    <View style={styles.main} {...props}>
+      <View style={flexBoxStyle({flex, direction, gap, justify, align})}>{props.children}</View>
     </View>
   );
 };
--- src/components/common/Flex/type.d.ts
+++ src/components/common/Flex/type.d.ts
@@ -1,7 +1,13 @@
-export type FlexProps = {
+import {ReactNode} from 'react';
+
+export class FlexProps {
   flex: number;
   direction: 'row' | 'column' | 'row-reverse' | 'column-reverse';
   gap: number;
   justify: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly';
   align: 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'space-between' | 'space-around' | 'space-evenly';
-};
+}
+
+export class FlexViewProps extends FlexProps {
+  children: ReactNode;
+}
